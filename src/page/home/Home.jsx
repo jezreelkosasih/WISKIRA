05896b20b0b625e546ab91fd13591ee79c64cf07
KoongHiap <yapkng@gmail.com>
2023-05-05 20:46:20 +0700

    add: bg-gradient1

---
+++ src/page/home/Home.jsx
@@ -0,0 +1,17 @@
+import "./home.css";
+
+const Home = () => {
+  return (
+    <>
+      <div className="Home">
+        <div className="bg1">
+          <div className="circle3"></div>
+          <div className="circle1"></div>
+          <div className="circle2"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { Home as Home };
